a95faefb99115ef1847a5f6af3fef6ae8309f9c5
Jesus Camarena <jesus.camarena-1@ou.edu>
2020-11-09 16:16:35 -0600

    add Cake.java

---
+++ src/Cake.java
@@ -0,0 +1,19 @@
+
+public class Cake {
+
+	private int bites = 10;
+	
+	public Cake() {
+		//nothing to implement
+	}
+	
+	public void takeABite() {
+		--bites;
+		System.out.println("Yumm...");
+	}
+	
+	public boolean isEmpty() {
+		return bites == 0;
+		
+	}
+}
